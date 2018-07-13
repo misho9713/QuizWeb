085c0a2565f5a1a87a184ec1624655bcc5d67f57
Misho <msokh15@freeuni.edu.ge>
2018-07-13 14:39:52 +0400

    changed the way server connects to database

--- src/Domain/ServerConnect.java
+++ src/Domain/ServerConnect.java
@@ -2,13 +2,31 @@ package Domain;
 
 import org.apache.commons.dbcp.BasicDataSource;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.FileInputStream;
+
 public class ServerConnect {
     public static BasicDataSource getDataSource() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("org.apache.commons.pool.KeyedObjectPoolFactory");
+        } catch (ClassNotFoundException ignored) {
+        }
+
         BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/test_db?characterEncoding=UTF8");
-        ds.setUsername("root");
-        ds.setPassword("1234578");
+
+        try (JsonReader reader = Json.createReader(new FileInputStream("DB/dbinfo.json"))) {
+            JsonObject info = reader.readObject();
+            ds.setUrl(info.getString("serverUrl"));
+            ds.setUsername(info.getString("username"));
+            if (!info.isNull("password")) ds.setPassword(info.getString("passwordF"));
+        } catch (Exception e) {
+            ds.setUrl("jdbc:mysql://localhost:3306/test_db?characterEncoding=UTF8");
+            ds.setUsername("root");
+        }
         return ds;
     }
 }
