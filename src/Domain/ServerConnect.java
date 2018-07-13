d2bc65855d29d9697af456a8edf9f42658f47660
misho9713 <msokh15@freeuni.edu.ge>
2018-07-13 16:44:45 +0400

    changed the way app connects to database

--- src/Domain/ServerConnect.java
+++ src/Domain/ServerConnect.java
@@ -1,14 +1,26 @@
 package Domain;
 
+import misc.DBConstants;
 import org.apache.commons.dbcp.BasicDataSource;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import java.io.FileInputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 
 public class ServerConnect {
-    public static BasicDataSource getDataSource() {
+    private static ServerConnect instance;
+
+    public static ServerConnect getInstance() {
+        return instance == null ? instance = new ServerConnect() : instance;
+    }
+
+    private static BasicDataSource getDataSource() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Class.forName("org.apache.commons.pool.KeyedObjectPoolFactory");
@@ -28,5 +40,60 @@ public class ServerConnect {
             ds.setUsername("root");
         }
         return ds;
+    }
+
+    private final BasicDataSource datasource;
+
+    private ServerConnect() {
+        this.datasource = getDataSource();
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+
+        return executeQuery(query, Collections.emptyList());
+    }
+
+    public int executeUpdate(String query) throws SQLException {
+        return executeUpdate(query, Collections.emptyList());
+    }
+
+    public ResultSet executeQuery(String query, List<Object> parameters) throws SQLException {
+
+        return (ResultSet) execute(query, false, parameters);
+    }
+
+    public int executeUpdate(String query, List<Object> parameters) throws SQLException {
+        return (int) execute(query, true, parameters);
+    }
+
+    private Object execute(String query, boolean update, List<Object> parameters) throws SQLException {
+        PreparedStatement statement = datasource.getConnection().prepareStatement(query);
+        try {
+            statement.executeQuery("USE " + DBConstants.MYSQL_DATABASE_NAME + ";");
+        } catch (SQLException e) {
+            try {
+                statement.close();
+            } catch (SQLException ignore) {
+
+            }
+        }
+        for (int i = 0; i < parameters.size(); i++) {
+            statement.setObject(i + 1, parameters.get(i));
+        }
+        if (update) {
+            int result = 0;
+            try {
+                result = statement.executeUpdate();
+            } catch (SQLException e) {
+                try {
+                    statement.close();
+                } catch (SQLException ignored) {
+
+                }
+            }
+            statement.close();
+            return result;
+        }
+        return statement.executeQuery();
     }
 }
---
+++ src/misc/DBConstants.java
@@ -0,0 +1,9 @@
+package misc;
+
+public final class DBConstants {
+    public static final String MYSQL_DATABASE_NAME = "quizdb";
+
+    private DBConstants() {
+
+    }
+}
