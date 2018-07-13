bcd78aae943146c920b34b40834f49709c7489c1
Misho <msokh15@freeuni.edu.ge>
2018-07-13 14:50:43 +0400

    rewrote database DDL

--- DB/Quizzddl.sql
+++ DB/Quizzddl.sql
@@ -1,68 +1,120 @@
-drop schema if exists QuizDB;
-create schema QuizDB;
-use QuizDB;
-create table Users(
-   ID int primary key auto_increment,
-   
-   UserT_ID int references UserTypes(ID),
-   
-   login_name nvarchar(50) unique not null,
-   
-   user_password  text
+DROP SCHEMA IF EXISTS QuizDB;
+CREATE SCHEMA QuizDB;
+USE QuizDB;
+
+CREATE TABLE quizzes
+(
+  quiz_id                  INT PRIMARY KEY AUTO_INCREMENT,
+  quiz_name                VARCHAR(150),
+  is_random                BOOLEAN NOT NULL,
+  is_single_page           BOOLEAN NOT NULL,
+  has_immediate_correction BOOLEAN NOT NULL,
+  can_practice             BOOLEAN NOT NULL
+);
+CREATE TABLE questions
+(
+  question_id     INT PRIMARY KEY                                           AUTO_INCREMENT,
+  quiz_id         INT NOT NULL,
+  question_text   TEXT,
+  question_type   ENUM ('text', 'fill-blank', 'multiple-choice', 'picture') DEFAULT 'text',
+  question_timer  INT                                                       DEFAULT 0,
+  CONSTRAINT questions_quizzes_quiz_id_fk FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id)
+    ON DELETE CASCADE,
+  answer_count    INT                                                       DEFAULT 1,
+  question_weight INT                                                       DEFAULT 1
+);
+CREATE TABLE question_answers
+(
+  answer_id   INT PRIMARY KEY  AUTO_INCREMENT,
+  question_id INT  NOT NULL,
+  answer      TEXT NOT NULL,
+  is_correct  BOOLEAN          DEFAULT TRUE,
+  CONSTRAINT question_answers_questions_question_id_fk FOREIGN KEY (question_id) REFERENCES questions (question_id)
+    ON DELETE CASCADE
+);
+CREATE TABLE user
+(
+  user_id            INT PRIMARY KEY AUTO_INCREMENT,
+  user_name          VARCHAR(100) UNIQUE,
+  user_password_hash TEXT                           NOT NULL,
+  user_role          ENUM ('user', 'administrator') NOT NULL
 );
 
-create table Quizzes(
-   ID int primary key auto_increment,
-   
-   quizz_description text not null,
-   
-   random_question boolean,
-   
-   multiple_page boolean,
-   
-   immediate_correction boolean,
-   
-   practice_mode boolean,
-   
-   User_ID  int not null references Users(ID),
-   
-   create_time datetime 
-   
+CREATE TABLE user_answer
+(
+  user_answer_id     INT PRIMARY KEY AUTO_INCREMENT,
+  user_id            INT NOT NULL,
+  question_id        INT NOT NULL,
+  question_answer_id INT,
+  user_answer_text   TEXT,
+  user_time_taken    INT             DEFAULT 0,
+  CONSTRAINT user_answer_user_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT user_answer_questions_question_id_fk FOREIGN KEY (question_id) REFERENCES questions (question_id)
 );
-create table QuestionTypes(
-  ID int auto_increment primary key,
-  
-  question_name  varchar(50)
+CREATE TABLE user_friendship
+(
+  friend_id        INT NOT NULL,
+  second_friend_id INT NOT NULL,
+  CONSTRAINT user_friendship_user_user_id_fk FOREIGN KEY (friend_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT user_friendship_user_user_id_fk_2 FOREIGN KEY (second_friend_id) REFERENCES user (user_id)
+    ON DELETE CASCADE
 );
-
-create table Questions(
-   ID int primary key auto_increment,
-   
-   QType_ID int not null references QuestionTypes(ID),
-   
-   question text 
+CREATE TABLE message
+(
+  message_id   INT PRIMARY KEY AUTO_INCREMENT,
+  sender_id    INT  NOT NULL,
+  reciever_id  INT  NOT NULL,
+  message_text TEXT NOT NULL,
+  CONSTRAINT message_user_user_id_fk FOREIGN KEY (sender_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT message_user_user_id_fk_2 FOREIGN KEY (reciever_id) REFERENCES user (user_id)
+    ON DELETE CASCADE
 );
-
-create table QuizzesTaken(
-
-    ID int auto_increment primary key,
-    
-    User_ID int not null references Users(ID),
-    
-    Quiz_ID int references Quizzes(ID),
-    
-    start_date datetime not null,
-    
-    end_date datetime not null,
-    
-	duration time not null,
-    
-    score decimal(5,2) not null
+CREATE TABLE challenge
+(
+  challenge_id   INT PRIMARY KEY AUTO_INCREMENT,
+  sender_id      INT NOT NULL,
+  reciever_id    INT NOT NULL,
+  quiz_id        INT NOT NULL,
+  challenge_text TEXT,
+  CONSTRAINT challenge_user_user_id_fk FOREIGN KEY (sender_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT challenge_user_user_id_fk_2 FOREIGN KEY (reciever_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT challenge_quizzes_quiz_id_fk FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id)
+    ON DELETE CASCADE
 );
-
-create table UserTypes(
-     ID int auto_increment primary key,
-     
-     user_type_name varchar(50)
-     
-)
+CREATE TABLE friend_request
+(
+  request_id      INT PRIMARY KEY AUTO_INCREMENT,
+  sender_id       INT NOT NULL,
+  reciever_id     INT NOT NULL,
+  request_message TEXT,
+  CONSTRAINT friend_request_user_user_id_fk FOREIGN KEY (sender_id) REFERENCES user (user_id)
+    ON DELETE CASCADE,
+  CONSTRAINT friend_request_user_user_id_fk_2 FOREIGN KEY (reciever_id) REFERENCES user (user_id)
+    ON DELETE CASCADE
+);
+CREATE TABLE quiz_taken
+(
+  quiz_taken_id INT PRIMARY KEY AUTO_INCREMENT,
+  quiz_id       INT  NOT NULL,
+  user_id       INT  NOT NULL,
+  quiz_start    DATE NOT NULL,
+  quiz_end      DATE NOT NULL,
+  CONSTRAINT quiz_taken_quizzes_quiz_id_fk FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id)
+    ON DELETE CASCADE,
+  CONSTRAINT quiz_taken_user_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id)
+    ON DELETE CASCADE
+);
+CREATE TABLE achievements
+(
+  achievement_id   INT PRIMARY KEY AUTO_INCREMENT,
+  user_id          INT                                                                  NOT NULL,
+  achievement_name ENUM ('Amateur Author', 'Prolific Author', 'Prodigious Author',
+                         'Quiz Machine', 'I am the Greatest', 'Practice Makes Perfect') NOT NULL,
+  CONSTRAINT achievements_user_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id)
+    ON DELETE CASCADE
+);
