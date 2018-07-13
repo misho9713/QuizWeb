589f8f78039708c51a8215fa5653a36a53031e76
Misho <msokh15@freeuni.edu.ge>
2018-07-13 19:48:21 +0400

    implemented user model

--- src/model/user/User.java
+++ src/model/user/User.java
@@ -3,10 +3,44 @@ package model.user;
 import model.common.Entity;
 
 public class User implements Entity {
-    private int id;
+    private final int id;
+    private final String name;
+    private final String password;
+    private final UserRole role;
+
+    /**
+     * A coonstructor reserved for adding entities into the database
+     */
+    public User(String name, String password, UserRole role) {
+        this(-1, name, password, role);
+    }
+
+    public User(int id, String name, String password, UserRole role) {
+
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
 
     @Override
     public int getId() {
         return id;
+    }
+
+    public enum UserRole {
+        USER, ADMINISTRATOR
     }
 }
