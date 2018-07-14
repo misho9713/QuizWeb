d910ec3c2909677ba9351ca1b4425c949daa7a03
Misho <msokh15@freeuni.edu.ge>
2018-07-14 12:51:33 +0400

    implemented getAllQuestionsOf(quiz)

--- src/model/quiz/QuizManager.java
+++ src/model/quiz/QuizManager.java
@@ -16,6 +16,30 @@ import java.util.List;
 import static misc.DBConstants.*;
 
 public class QuizManager implements EntityManager<Quiz, QuizSeeker> {
+    private static QuizManager instance;
+
+    public static Question getQuestion(ResultSet rs) throws SQLException {
+        Quiz quiz;
+        try {
+            quiz = getQuiz(rs);
+        } catch (SQLException e) {
+            quiz = getInstance().get(rs.getInt(DB_COLUMN_QUESTION_QUIZ_ID));
+        }
+        return new Question(
+                rs.getInt(DB_COLUMN_QUESTION_ID),
+                quiz,
+                rs.getString(DB_COLUMN_QUESTION_TEXT),
+                Question.QuestionType.valueOf(rs.getString(DB_COLUMN_QUESTION_TYPE).toUpperCase()),
+                rs.getLong(DB_COLUMN_QUESTION_TIMER),
+                rs.getInt(DB_COLUMN_QUESTION_ANSWER_COUNT),
+                rs.getInt(DB_COLUMN_QUESTION_WEIGHT)
+        );
+    }
+
+    public static QuizManager getInstance() {
+        return instance == null ? instance = new QuizManager() : instance;
+    }
+
     private static Quiz getQuiz(ResultSet results) throws SQLException {
         User author;
         try {
@@ -67,6 +91,7 @@ public class QuizManager implements EntityManager<Quiz, QuizSeeker> {
         return false;
     }
 
+
     @Override
     public Quiz get(int id) {
         String sql = String.format("SELECT *\n FROM %s\n  INNER JOIN %s u ON %s.%s = u.%s WHERE %s = ?",
@@ -90,7 +115,7 @@ public class QuizManager implements EntityManager<Quiz, QuizSeeker> {
                 DB_TABLE_QUESTION, DB_TABLE_QUIZ, DB_TABLE_QUESTION, DB_COLUMN_QUESTION_QUIZ_ID, DB_COLUMN_QUIZ_ID, DB_COLUMN_QUIZ_ID);
         try (ResultSet rs = ServerConnect.getInstance().executeQuery(query, Collections.singletonList(quiz.getId()))) {
             while (rs.next()) {
-
+                questions.add(getQuestion(rs));
             }
         } catch (Exception ignored) {
 
